bbb3a27f6cb9459ffd9bd486c915ef2c7837c883
Rajendra Kumar R Yadav <rajendra.ecti@gmail.com>
2020-02-14 11:44:29 +0530

    Updated 09.c, to Check wheather file exists or not

--- 09.c
+++ 09.c
@@ -6,9 +6,14 @@
 int main(int argc, char **argv)
 {
     struct stat file_stat;
+    FILE *file;
     for (int i = 1; i < argc; i++)
     {
         lstat(argv[i], &file_stat);
+        if(fopen(argv[i], "r") == NULL) {
+            perror("Error   ");
+            exit(EXIT_FAILURE);
+        }
         if (((&file_stat)->st_size) > 1024)
         {
             printf("File Name : %4s, File Size : %0.2lf KiloByte\n", argv[i], (double)(&file_stat)->st_size / 1024);
