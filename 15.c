13c4ceea90b75c5276ff1e86baf77dbba8dff9e2
Rajendra Kumar R Yadav <rajendra.ecti@gmail.com>
2020-02-18 14:38:18 +0530

    15.c Completed!

--- 15.c
+++ 15.c
@@ -23,9 +23,27 @@ void printInfo(struct stat *filestat, const char *fileName)
 {
   struct passwd *pwd;
   struct group *grp;
+  char f_created_time[50];
+
   grp = getgrgid(filestat->st_gid);
   pwd = getpwuid(filestat->st_uid);
-  printf("%lu\t%12.32s\t%12.32s\t%ld\t\n",
+  struct tm c_time;
+  c_time = *localtime(&filestat->st_ctime);
+  strftime(f_created_time, sizeof(f_created_time), "%b %2d %R", &c_time);
+
+  printf((S_ISDIR(filestat->st_mode)) ? "d" : "-");
+  printf((filestat->st_mode & S_IRUSR) ? "r" : "-");
+  printf((filestat->st_mode & S_IWUSR) ? "w" : "-");
+  printf((filestat->st_mode & S_IXUSR) ? "x" : "-");
+  printf((filestat->st_mode & S_IRGRP) ? "r" : "-");
+  printf((filestat->st_mode & S_IWGRP) ? "w" : "-");
+  printf((filestat->st_mode & S_IXGRP) ? "x" : "-");
+  printf((filestat->st_mode & S_IROTH) ? "r" : "-");
+  printf((filestat->st_mode & S_IWOTH) ? "w" : "-");
+  printf((filestat->st_mode & S_IXOTH) ? "x" : "-");
+  printf(" ");
+
+  printf("%2lu %14.32s %14.32s % ld %12.14s %s\n",
          filestat->st_nlink, pwd->pw_name,
-         grp->gr_name, filestat->st_size);
+         grp->gr_name, filestat->st_size, f_created_time, fileName);
 }
