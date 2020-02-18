78b1ad26cd391e972b0ff1112f02e5b82b5bf5cc
Rajendra Kumar R Yadav <rajendra.ecti@gmail.com>
2020-02-18 13:59:36 +0530

    Update in 15.c

--- 15.c
+++ 15.c
@@ -1,32 +1,31 @@
-#include <time.h>
-#include <stdio.h>
+#include <dirent.h>
 #include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <grp.h>
+#include <pwd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <dirent.h>
-#include <pwd.h>
-#include <grp.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+void printInfo(struct stat *, const char *);
 
-int main(int argc, char **argv)
+int main(int argc, const char **argv)
 {
-    FILE *file;
-    DIR *dir;
-    struct dirent *dirent;
-    struct stat filestat;
-    struct passwd *pwd;
-    struct group *grp;
+  struct stat filestat;
+  stat(argv[1], &filestat);
+  printInfo(&filestat, argv[1]);
+  return EXIT_SUCCESS;
+}
 
-    if (argc > 2)
-    {
-        dir = opendir(argv[1]);
-        if (dir == NULL)
-        {
-            perror("Error ");
-        }
-    }
-
-    return EXIT_SUCCESS;
+void printInfo(struct stat *filestat, const char *fileName)
+{
+  struct passwd *pwd;
+  struct group *grp;
+  grp = getgrgid(filestat->st_gid);
+  pwd = getpwuid(filestat->st_uid);
+  printf("%lu\t%12.32s\t%12.32s\t%ld\t\n",
+         filestat->st_nlink, pwd->pw_name,
+         grp->gr_name, filestat->st_size);
 }
--- Makefile
+++ Makefile
@@ -1,12 +1,11 @@
 CC=gcc
-NAME=07
+NAME=15
 FILES=${NAME}.c
 FLAGS=--std=c11 -Wall -g
 OUTPUT=./bin/${NAME}
 
 main: ${FILES}
 	${CC} ${FILES} ${FLAGS} -o ${OUTPUT}
-	-clear
 
 run: main ${FILES}
 	${OUTPUT}
