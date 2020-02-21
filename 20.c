465f4e475fffb3c166660a91c30b5e0175b69755
Rajendra Kumar R Yadav <rajendra.ecti@gmail.com>
2020-02-21 21:04:02 +0530

    20.c

--- 20.c
+++ 20.c
@@ -4,18 +4,12 @@
  * @file 20.c                                                     *
  **************************************************************** */
 #include <errno.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <unistd.h>
-int main (int argc, char *Argv[])
+
+int main (int argc, char *argv[])
 {
-    if (argc < 3)
-        {
-            printf ("Usage : %s <filename1> <filename2> ...\n", Argv[0]);
-            exit (-1);
-        }
+    errno = EACCES;
+    perror (argv[0]);
     exit (EXIT_SUCCESS);
 }
